e206a0afd522abf6b8a79c6d61c095b2c5d80fcd
Stevenlin55 <70299036+Stevenlin55@users.noreply.github.com>
2023-02-14 21:42:05 -0600

    Create skeleton for individual projects to be shown on Projects Screen

--- Screens/ProjectsPageScreen.js
+++ Screens/ProjectsPageScreen.js
@@ -1,11 +1,73 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
+// import hat image from images folder
+import hat from "../images/flower-bucket-hat.jpg";
 
 function ProjectsPageScreen() {
-    return (
-        <View>
-            <Text>ProjectsPageScreen</Text>
+  return (
+    <View style={styles.container}>
+      <View style={styles.project}>
+        <View style={{ flexDirection: "row" }}>
+          <View style={{ width: "60%" }}>
+            <Text style={{ fontWeight: "bold" }}>Flower Bucket Hat</Text>
+            <View style={styles.projectInfo}>
+              <Text style={styles.projectInfoText}>Type: </Text>
+              <Text style={styles.projectInfoText}>Tool(s): </Text>
+              <Text style={styles.projectInfoText}>Materials: </Text>
+              <Text style={styles.projectInfoText}>Pattern: </Text>
+              <Text style={styles.projectInfoText}>Description: </Text>
+            </View>
+          </View>
+          <View
+            style={{
+              width: "40%",
+              flex: 1,
+              alignItems: "center",
+              justifyContent: "center",
+              padding: 15,
+            }}
+          >
+            <Image style={styles.image} source={hat} />
+            <Text style={styles.postedStatus}>POSTED</Text>
+          </View>
         </View>
-    );
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: "white", //GlobalStyles.colors.primary50,
+  },
+
+  project: {
+    borderRadius: 10,
+    backgroundColor: "#F5F5F5",
+    padding: 10,
+  },
+
+  projectInfo: {
+    marginTop: 5,
+    marginBottom: 5,
+  },
+
+  projectInfoText: {
+    fontSize: 11,
+  },
+
+  image: {
+    width: 120,
+    height: 110,
+    borderRadius: 10,
+  },
+
+  postedStatus: {
+    fontSize: 14,
+    fontWeight: "bold",
+    marginTop: 20,
+  },
+});
 
 export default ProjectsPageScreen;
