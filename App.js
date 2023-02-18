40abf58ae2bc95546df01c02880cd605c99bbe15
Brandon Li <li.brandon@outlook.com>
2023-02-18 15:42:07 -0600

    Update icons, add profile icon, create Edit Profile screen

--- App.js
+++ App.js
@@ -1,91 +1,140 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import FrontPageScreen from './Screens/FrontPageScreen';
-import ExplorePageScreen from './Screens/ExplorePageScreen';
-import AddProjectScreen from './Screens/AddProjectScreen';
-import ManageExpensesScreen from './Screens/ManageExpensesScreen';
-import InventoryPageScreen from './Screens/InventoryPageScreen';
-import ItemDetailScreen from './Screens/ItemDetailScreen';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
-import { GlobalStyles } from './Constants/styles';
-import { Ionicons } from '@expo/vector-icons';
-import { store } from './Data/store';
-import { Provider } from 'react-redux';
-import IconButton from './Components/UI/IconButton';
-import ExpensesContextProvider from './Data/expenses-context';
-import ProjectsPageScreen from './Screens/ProjectsPageScreen';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import FrontPageScreen from "./Screens/FrontPageScreen";
+import ExplorePageScreen from "./Screens/ExplorePageScreen";
+import AddProjectScreen from "./Screens/AddProjectScreen";
+import ManageExpensesScreen from "./Screens/ManageExpensesScreen";
+import InventoryPageScreen from "./Screens/InventoryPageScreen";
+import ItemDetailScreen from "./Screens/ItemDetailScreen";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { GlobalStyles } from "./Constants/styles";
+import { Ionicons } from "@expo/vector-icons";
+import { store } from "./Data/store";
+import { Provider } from "react-redux";
+import IconButton from "./Components/UI/IconButton";
+import ExpensesContextProvider from "./Data/expenses-context";
+import ProjectsPageScreen from "./Screens/ProjectsPageScreen";
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+
 function ToManage(navigation) {
-  return navigation.navigate('Manage Page');
+  return navigation.navigate("Manage Page");
 }
+
+function ToProfile(navigation) {
+  return navigation.navigate("Edit Profile");
+}
+
 const FrontPage = function () {
   const isUser = true;
   return (
-    <Tab.Navigator screenOptions={({ navigation }) => ({
-      headerStyle: { backgroundColor: GlobalStyles.colors.primary50, },
-      headerTintColor: GlobalStyles.colors.gray700,
-      tabBarStyle: { backgroundColor: GlobalStyles.colors.primary50, },
-      tabBarActiveTintColor: GlobalStyles.colors.gray700,
-      headerRight: isUser ? ({ tintColor }) => (
-        <IconButton icon="add" color={tintColor} size={24} onPress={ToManage.bind(this, navigation)} />
-        // <IconButton name="add" color={tintColor} size={24} onPress={ToManage} />
-      ) : null,
-    })}>
-      <Tab.Screen name="Craft Cache" component={FrontPageScreen}
+    <Tab.Navigator
+      screenOptions={({ navigation }) => ({
+        headerStyle: { backgroundColor: GlobalStyles.colors.primary50 },
+        headerTintColor: GlobalStyles.colors.gray700,
+        tabBarStyle: { backgroundColor: GlobalStyles.colors.primary50 },
+        tabBarActiveTintColor: GlobalStyles.colors.gray700,
+        headerRight: isUser
+          ? ({ tintColor }) => (
+              <IconButton
+                icon="log-in-outline"
+                color={tintColor}
+                size={24}
+                onPress={ToManage.bind(this, navigation)}
+              />
+            )
+          : null,
+        headerLeft: isUser
+          ? ({ tintColor }) => (
+              <IconButton
+                icon="person-circle-outline"
+                color={tintColor}
+                size={24}
+                onPress={ToProfile.bind(this, navigation)}
+              />
+            )
+          : null,
+      })}
+    >
+      <Tab.Screen
+        name="Home"
+        component={FrontPageScreen}
         options={{
-          tabBarIcon: ({ color, size }) => <Ionicons name='home-outline' size={size} color={color} />
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" size={size} color={color} />
+          ),
         }}
       />
-      <Tab.Screen name="Explore" component={ExplorePageScreen}
+      <Tab.Screen
+        name="Explore"
+        component={ExplorePageScreen}
         options={{
-          tabBarIcon: ({ color, size }) => <Ionicons name='eye-outline' size={size} color={color} />
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="eye-outline" size={size} color={color} />
+          ),
         }}
       />
-      <Tab.Screen name="AddProjectScreen" component={AddProjectScreen}
+      <Tab.Screen
+        name="Add Project"
+        component={AddProjectScreen}
         options={{
-          tabBarIcon: ({ color, size }) => <Ionicons name='add-circle-outline' size={size} color={color} />
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="add-circle-outline" size={size} color={color} />
+          ),
         }}
       />
-      <Tab.Screen name="ProjectsPageScreen" component={ProjectsPageScreen}
+      <Tab.Screen
+        name="Projects"
+        component={ProjectsPageScreen}
         options={{
-          tabBarIcon: ({ color, size }) => <Ionicons name='folder-outline' size={size} color={color} />
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="folder-outline" size={size} color={color} />
+          ),
         }}
       />
-      <Tab.Screen name="InventoryPageScreen" component={InventoryPageScreen}
+      <Tab.Screen
+        name="Inventory"
+        component={InventoryPageScreen}
         options={{
-          tabBarIcon: ({ color, size }) => <Ionicons name='cube-outline' size={size} color={color} />
-        }} />
-
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="cube-outline" size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
-}
+};
 export default function App() {
   return (
     <Provider store={store}>
       {/* <ExpensesContextProvider> */}
       <NavigationContainer>
-        <Stack.Navigator screenOptions={{
-          headerStyle: { backgroundColor: GlobalStyles.colors.primary500, },
-          headerTintColor: 'white',
-          tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500, },
-          tabBarActiveTintColor: GlobalStyles.colors.accent500,
-        }}>
-          <Stack.Screen name="Front Page" component={FrontPage}
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+            headerTintColor: "white",
+            tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+            tabBarActiveTintColor: GlobalStyles.colors.accent500,
+          }}
+        >
+          <Stack.Screen
+            name="Front Page"
+            component={FrontPage}
             options={{
               headerShown: false,
-              headerBackVisible: false
+              headerBackVisible: false,
             }}
           />
-          <Stack.Screen name="Manage Page" component={ManageExpensesScreen}
+          <Stack.Screen
+            name="Manage Page"
+            component={ManageExpensesScreen}
             options={{
-              presentation: 'modal'
+              presentation: "modal",
             }}
           />
-          <Stack.Screen name="Detail" component={ItemDetailScreen}
-          />
+          <Stack.Screen name="Detail" component={ItemDetailScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
@@ -96,8 +145,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
