443862f247bc0df1133d2479992f0a3d0e2ac280
Stevenlin55 <70299036+Stevenlin55@users.noreply.github.com>
2023-02-19 19:44:22 -0600

    Briefly connected firestore to Projects screen

--- Screens/ProjectsPageScreen.js
+++ Screens/ProjectsPageScreen.js
@@ -2,9 +2,24 @@ import { ScrollView, Text, StyleSheet, AsyncStorage } from "react-native";
 import React, { useEffect, useState, createContext, useContext } from "react";
 import Project from "../Components/ProjectsPage/Project";
 import { MyContext } from "../Contexts/MyContext";
+import { auth, db } from "../firebase";
+
+import { collection, getDocs, query, where } from "firebase/firestore";
 
 function ProjectsPageScreen({ navigation }) {
   const { projects, setProjects } = useContext(MyContext);
+
+  const user = "9WMhukGezkVDYncWcfTV"; // TODO: user id is hardcoded for now
+
+  useEffect(() => {
+    // fetch projects from database for the current user
+    const q = query(collection(db, "projects"), where("user", "==", user));
+    getDocs(q).then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        console.log(doc.data());
+      });
+    });
+  }, []);
 
   return (
     // TODO: Change ScrollView to FlatList
