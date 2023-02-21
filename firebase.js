d115663a0dd229a104794194c3f26e0d681a3afd
leting sun <leting@wustl.edu>
2023-02-21 08:37:01 -0600

    Front page list changed
    just change the list in the front page to the project page's list

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import * as firebase from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -25,4 +25,11 @@ if (firebase.getApps().length === 0) {
 // const auth = firebase.auth()
 const auth = getAuth();
 const db = getFirestore();
+async function getUser(db) {
+    const usercol = collection(db, 'projects');
+    const snapshot = await getDocs(usercol);
+    const list = snapshot.docs.map(doc => doc.data());
+    console.log(list);
+}
+getUser(db);
 export { auth, db }
