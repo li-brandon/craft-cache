98236dde572dad0650e7d9965415d4f3359fd486
Brandon Li <li.brandon@outlook.com>
2023-02-23 12:05:44 -0600

    Create barebones User Profile screen

--- Screens/UserProfileScreen.js
+++ Screens/UserProfileScreen.js
@@ -1,72 +1,101 @@
-import { StyleSheet, Text, TouchableOpacity, View, Button } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Button,
+  Image,
+  ScrollView,
+  FlatList,
+} from "react-native";
+import React, { useState, createContext, useContext } from "react";
 import { getAuth, signOut } from "firebase/auth";
-import { auth } from '../firebase';
-const UserProfileScreen = ({ navigation, route }) => {
+import { auth } from "../firebase";
+import Project from "../Components/ProjectsPage/Project";
+import { MyContext } from "../Contexts/MyContext";
 
-  function UserProfileScreen({ navigation }) {
+import hat from "../Components/assets/flower-bucket-hat.jpg";
+
+const UserProfileScreen = ({ navigation, route }) => {
+  // TODO: Verify if the user is logged in or not and display appropriate screen
+
+  const { projects, setProjects } = useContext(MyContext);
+
   return (
     <View>
-      <Text>User Profile Screen</Text>
       <Button
         title="Edit Profile"
         onPress={() => navigation.navigate("Edit Profile")}
       />
+      <Image style={styles.image} source={hat} />
+      <Text>Username</Text>
+      <Text>Published Projects:</Text>
+
+      <ScrollView contentContainerStyle={styles.wrapper}>
+        {/* map over projects and render Project component */}
+        {projects.map((project, index) => (
+          <Project key={index} project={project} navigation={navigation} />
+        ))}
+      </ScrollView>
     </View>
   );
-}
 
-    // const email = route.params['email'];
-    const signOutHandler = () => {
-        signOut(auth).then(() => {
-            console.log('Sign-out successful.');
-            navigation.replace('Front Page');
-        }).catch((error) => {
-            // An error happened.
-        });
-    }
-    const handleBack = function () {
-        navigation.goBack();
-    }
-    return (
-        <View style={styles.container}>
-            <Text>email:{auth.currentUser?.email}</Text>
-            <TouchableOpacity
-                onPress={signOutHandler}
-                style={styles.button}
-            >
-                <Text style={styles.buttonText}>sign out</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                onPress={handleBack}
-                style={styles.button}
-            >
-                <Text style={styles.buttonText}>To Front Page</Text>
-            </TouchableOpacity>
+  // // const email = route.params['email'];
+  // const signOutHandler = () => {
+  //     signOut(auth).then(() => {
+  //         console.log('Sign-out successful.');
+  //         navigation.replace('Front Page');
+  //     }).catch((error) => {
+  //         // An error happened.
+  //     });
+  // }
+  // const handleBack = function () {
+  //     navigation.goBack();
+  // }
+  // return (
+  //     <View style={styles.container}>
+  //         <Text>email:{auth.currentUser?.email}</Text>
+  //         <TouchableOpacity
+  //             onPress={signOutHandler}
+  //             style={styles.button}
+  //         >
+  //             <Text style={styles.buttonText}>sign out</Text>
+  //         </TouchableOpacity>
+  //         <TouchableOpacity
+  //             onPress={handleBack}
+  //             style={styles.button}
+  //         >
+  //             <Text style={styles.buttonText}>To Front Page</Text>
+  //         </TouchableOpacity>
 
-        </View >
-    )
-}
+  //     </View >
+  // )
+};
 
-export default UserProfileScreen
+export default UserProfileScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        flex: 1,
-    },
-    button: {
-        backgroundColor: '#0728f9',
-        width: '100%',
-        padding: 15,
-        margin: 10,
-        borderRadius: 10,
-        alignItems: 'center',
-    },
-    buttonText: {
-        color: 'white',
-        fontWeight: '700',
-        fontSize: 16,
-    },
-})
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+  },
+  button: {
+    backgroundColor: "#0728f9",
+    width: "100%",
+    padding: 15,
+    margin: 10,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+  },
+});
