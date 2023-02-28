35497b34aeb6fffdb183bf773aaeee9dc8921a0a
Brandon Li <li.brandon@outlook.com>
2023-02-28 17:06:18 -0600

    Add RNUILib

---
+++ node_modules/react-native-ui-lib/src/incubator/TextField/Label.js
@@ -0,0 +1,42 @@
+import React, { useContext, useMemo } from 'react';
+import { StyleSheet } from 'react-native';
+import { Colors } from "../../style";
+import Text from "../../components/text";
+import { ValidationMessagePosition } from "./types";
+import { getColorByState } from "./Presenter";
+import FieldContext from "./FieldContext";
+const DEFAULT_LABEL_COLOR = {
+  default: Colors.$textDefault,
+  readonly: Colors.$textNeutral
+};
+const Label = ({
+  label,
+  labelColor = DEFAULT_LABEL_COLOR,
+  labelStyle,
+  labelProps,
+  validationMessagePosition,
+  floatingPlaceholder,
+  testID
+}) => {
+  const context = useContext(FieldContext);
+  const forceHidingLabel = !context.isValid && validationMessagePosition === ValidationMessagePosition.TOP;
+  const style = useMemo(() => {
+    return [styles.label, labelStyle, floatingPlaceholder && styles.dummyPlaceholder];
+  }, [labelStyle, floatingPlaceholder]);
+  if ((label || floatingPlaceholder) && !forceHidingLabel) {
+    return <Text testID={testID} color={getColorByState(labelColor, context)} style={style} {...labelProps}>
+        {label}
+      </Text>;
+  }
+  return null;
+};
+const styles = StyleSheet.create({
+  label: {
+    minHeight: 20
+  },
+  dummyPlaceholder: {
+    opacity: 0
+  }
+});
+Label.displayName = 'Incubator.TextField';
+export default Label;
---
+++ node_modules/react-native-ui-lib/typings/index.d.ts
@@ -0,0 +1,8 @@
+
+export * from './assets';
+export * from './commons';
+export * from './components';
+export * from './incubator';
+export * from './generatedTypes';
+export * from './globalTypes';
+export * from './hooks';
