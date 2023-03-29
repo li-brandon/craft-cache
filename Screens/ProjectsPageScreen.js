262d0d1c88dab55885a720a33a78c7d95b60a1f1
leting sun <leting@wustl.edu>
2023-03-29 13:54:08 -0500

    can choose simple cagory to filter

--- Screens/ProjectsPageScreen.js
+++ Screens/ProjectsPageScreen.js
@@ -1,15 +1,19 @@
-import { ScrollView, Text, StyleSheet, AsyncStorage } from "react-native";
+import { ScrollView, StyleSheet, AsyncStorage } from "react-native";
 import React, { useState, useEffect, createContext, useContext } from "react";
 import { useFocusEffect } from "@react-navigation/native";
 import Project from "../Components/ProjectsPage/Project";
 import { MyContext } from "../Contexts/MyContext";
 import { auth, db } from "../firebase";
-
+import { TouchableOpacity } from "react-native";
+import { Component } from 'react';
+import { View, TextField, Text, Button, Colors } from 'react-native';
 import { collection, getDocs, query, where } from "firebase/firestore";
-
+// import { Checkox } from '@react-native-community/checkbox;
 function ProjectsPageScreen({ navigation }) {
   const { projects, setProjects } = useContext(MyContext);
   const [currentUser, setCurrentUser] = useState(null);
+  const [posted, setIsPosted] = useState(true);
+  const [categoryFilter, setCategoryFilter] = useState(["Embroidery", "Crochet", "Knitting", "Sewing"]);
 
   useEffect(() => {
     // get current user
@@ -31,8 +35,12 @@ function ProjectsPageScreen({ navigation }) {
 
         const q = query(
           collection(db, "projects"),
-          where("userID", "==", userID)
+          where("userID", "==", userID),
+          where("posted", "==", posted),
+          // where("type", "array-contains", "Embroidery"),
+          where("type", "array-contains-any", categoryFilter),
         );
+
         getDocs(q).then((querySnapshot) => {
           querySnapshot.forEach((doc) => {
             tempProjects.push({ ...doc.data(), id: doc.id });
@@ -42,17 +50,34 @@ function ProjectsPageScreen({ navigation }) {
       } else {
         setProjects([]); // clear projects since there is no user
       }
-    }, [currentUser])
+    }, [currentUser, posted, categoryFilter])
   );
 
   return (
-    // TODO: Change ScrollView to FlatList
-    <ScrollView style={styles.container}>
-      {/* map over projects and render Project component */}
-      {projects.map((project, index) => (
-        <Project key={index} project={project} navigation={navigation} />
-      ))}
-    </ScrollView>
+    <View style={styles.container}>
+
+      <Button label={'Press'}
+        onPress={() => setIsPosted(!posted)}
+        title="unposted" ></Button>
+      <Button label={'Press'}
+        onPress={() => setCategoryFilter(["Embroidery"])}
+        title="Embroidery" ></Button>
+      <Button label={'Press'}
+        onPress={() => setCategoryFilter(["Sewing"])}
+        title="Sewing" ></Button>
+      <Button label={'Press'}
+        onPress={() => setCategoryFilter(["Embroidery", "Crochet", "Knitting", "Sewing"])}
+        title="All" ></Button>
+      {/* <CheckBox value={!posted}
+        onValueChange={() => setIsPosted(!posted)}
+        label="unposted" /> */}
+
+      <ScrollView style={styles.container}>
+        {projects.map((project, index) => (
+          <Project key={index} project={project} navigation={navigation} />
+        ))}
+      </ScrollView>
+    </View>
   );
 }
 
