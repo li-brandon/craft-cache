5cd499485b07cfce5975e312ca6910393326125f
Brandon Li <brandonli@Brandons-MBP-2.attlocal.net>
2023-04-11 15:29:26 -0500

    Entered text is shown as appropriate comment

--- Screens/CommentsScreen.js
+++ Screens/CommentsScreen.js
@@ -1,51 +1,111 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, Button, FlatList } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+  StyleSheet,
+  Image,
+} from "react-native";
 
-const CommentsScreen = () => {
+import hat from "../Components/assets/flower-bucket-hat.jpg";
+
+const PostComments = () => {
+  const [enteredComment, setEnteredComment] = useState("");
   const [comments, setComments] = useState([]);
 
-  const handleCommentSubmit = (comment) => {
-    setComments([...comments, comment]);
+  const handleSubmitComment = (comment, profileIcon) => {
+    setComments([...comments, { comment, profileIcon }]);
   };
 
-  const handleReplySubmit = (reply, commentIndex) => {
-    const updatedComments = [...comments];
-    updatedComments[commentIndex].replies.push(reply);
-    setComments(updatedComments);
+  const renderItem = ({ item }) => {
+    return (
+      <View style={styles.commentContainer}>
+        <Image source={item.profileIcon} style={styles.profileIcon} />
+        <Text style={styles.commentText}>{item.comment}</Text>
+        <TouchableOpacity>
+          <Text style={styles.replyButton}>Reply</Text>
+        </TouchableOpacity>
+      </View>
+    );
   };
-
-  const renderItem = ({ item, index }) => (
-    <View>
-      <Text>{item.comment}</Text>
-      <FlatList
-        data={item.replies}
-        renderItem={({ item }) => <Text>{item}</Text>}
-        keyExtractor={(item, index) => index.toString()}
-      />
-      <TextInput
-        placeholder="Enter your reply"
-        onSubmitEditing={(e) => handleReplySubmit(e.nativeEvent.text, index)}
-      />
-    </View>
-  );
 
   return (
-    <View>
-      <Text>Comments</Text>
-      <TextInput
-        placeholder="Enter your comment"
-        onSubmitEditing={(e) =>
-          handleCommentSubmit({ comment: e.nativeEvent.text, replies: [] })
-        }
-      />
-      <Button title="Submit" onPress={() => {}} />
+    <View style={styles.container}>
       <FlatList
         data={comments}
         renderItem={renderItem}
         keyExtractor={(item, index) => index.toString()}
+        style={styles.commentList}
       />
+      <View style={styles.inputContainer}>
+        <Image source={hat} style={styles.profileIcon} />
+        <TextInput
+          placeholder="Add a comment..."
+          style={styles.input}
+          onChangeText={(newComment) => setEnteredComment(newComment)}
+        />
+        <TouchableOpacity
+          onPress={() => handleSubmitComment(enteredComment, hat)}
+        >
+          <Text style={styles.postButton}>Post</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
-export default CommentsScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingHorizontal: 16,
+  },
+  commentList: {
+    marginTop: 16,
+  },
+  commentContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 8,
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+  },
+  profileIcon: {
+    width: 24,
+    height: 24,
+    borderRadius: 12,
+    marginRight: 8,
+  },
+  commentText: {
+    flex: 1,
+    fontSize: 16,
+    marginLeft: 8,
+  },
+  replyButton: {
+    color: "#039be5",
+    fontSize: 16,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 16,
+    justifyContent: "center", // centers the child elements horizontally
+  },
+  input: {
+    flex: 1,
+    padding: 8,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 4,
+    marginRight: 16,
+    fontSize: 16,
+  },
+  postButton: {
+    color: "#039be5",
+    fontSize: 16,
+  },
+});
+
+export default PostComments;
