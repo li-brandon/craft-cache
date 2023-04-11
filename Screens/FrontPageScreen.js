80ac647d98f3b762f74070966c3d2442576113dd
Brandon Li <brandonli@Brandons-MBP-2.attlocal.net>
2023-04-11 15:01:57 -0500

    Remove outdated imports

--- Screens/FrontPageScreen.js
+++ Screens/FrontPageScreen.js
@@ -1,7 +1,5 @@
 import { View, Text, Button, StyleSheet } from "react-native";
-import ExpensesOutput from "../Components/ExpensesOutput/ExpensesOutput";
-import { Dummy_expenses } from "../Data/ExpensesData";
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from "react-redux";
 import { useContext, useState, useEffect } from "react";
 import { ExpensesContext } from "../Data/expenses-context";
 import HomePageScreen from "./HomePageScreen";
@@ -9,32 +7,35 @@ import { GlobalStyles } from "../Constants/styles";
 import { auth, db } from "../firebase";
 import { LoginContext } from "../Contexts/LoginContext";
 function FrontPageScreen({ navigation, route }) {
-    const { loggedIn, setloggedIn } = useContext(LoginContext);
-    return (
-        <View style={styles.container}>
-            {/* if user logged in, show HomePageScreen. Otherwise redirect user to Login */}
-            {loggedIn ? <HomePageScreen navigation={navigation} /> : navigation.navigate('Login')}
-
-        </View>
-    );
-};
+  const { loggedIn, setloggedIn } = useContext(LoginContext);
+  return (
+    <View style={styles.container}>
+      {/* if user logged in, show HomePageScreen. Otherwise redirect user to Login */}
+      {loggedIn ? (
+        <HomePageScreen navigation={navigation} />
+      ) : (
+        navigation.navigate("Login")
+      )}
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignContent: 'center'
-    },
-    textContainer: {
-        margin: 10,
-        // justifyContent: 'center',
-        // alignContent: "center",
-    },
-    text: {
-        textAlign: 'center',
-        fontSize: 24,
-        fontWeight: 'bold',
-        color: GlobalStyles.colors.error500
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignContent: "center",
+  },
+  textContainer: {
+    margin: 10,
+    // justifyContent: 'center',
+    // alignContent: "center",
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 24,
+    fontWeight: "bold",
+    color: GlobalStyles.colors.error500,
+  },
+});
 export default FrontPageScreen;
