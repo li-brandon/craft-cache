985cba47fed60a9c7078db508107925cc143af0c
li-brandon <li.brandon@outlook.com>
2023-04-24 18:32:07 -0500

    Install react-native-gesture-handler

--- Screens/InventoryPageScreen.js
+++ Screens/InventoryPageScreen.js
@@ -112,3 +112,71 @@ const outerStyles = StyleSheet.create({
 });
 
 export default InventoryPageScreen;
+
+// import React, { useRef } from "react";
+// import {
+//   View,
+//   Text,
+//   TouchableOpacity,
+//   Animated,
+//   PanResponder,
+//   StyleSheet,
+// } from "react-native";
+
+// const SlideToDelete = () => {
+//   const pan = useRef(new Animated.ValueXY()).current;
+//   const panResponder = useRef(
+//     PanResponder.create({
+//       onMoveShouldSetPanResponderCapture: () => true,
+//       onPanResponderMove: Animated.event(
+//         [
+//           null,
+//           { dx: pan.x }, // Update pan.x as the user swipes
+//         ],
+//         { useNativeDriver: false }
+//       ),
+//       onPanResponderRelease: () => {
+//         // Add logic here for when the swipe is released, e.g. trigger delete action
+//         // You can use the value of pan.x to determine if the swipe was enough to trigger the delete action
+//         // Reset the pan value after the swipe is released
+//         Animated.spring(pan, {
+//           toValue: { x: 0, y: 0 },
+//           useNativeDriver: false,
+//         }).start();
+//       },
+//     })
+//   ).current;
+
+//   return (
+//     <View style={styles.container}>
+//       <Animated.View
+//         style={[styles.itemContainer, { transform: [{ translateX: pan.x }] }]} // Apply translation to the item based on pan.x value
+//         {...panResponder.panHandlers} // Pass panHandlers to enable pan gestures
+//       >
+//         <TouchableOpacity onPress={() => console.log("Item clicked")}>
+//           <Text style={styles.itemText}>Swipe to delete</Text>
+//         </TouchableOpacity>
+//       </Animated.View>
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: "center",
+//     alignItems: "center",
+//   },
+//   itemContainer: {
+//     backgroundColor: "red",
+//     height: 50,
+//     justifyContent: "center",
+//     paddingHorizontal: 16,
+//   },
+//   itemText: {
+//     color: "white",
+//     fontSize: 18,
+//   },
+// });
+
+// export default SlideToDelete;
